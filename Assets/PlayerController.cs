71a4c760b1523980cd5b5d27381a32b75f5de2a9
Tommy Andersson <Tommy.J.Andersson@nexergroup.com>
2024-01-21 16:11:03 +0100

    Lagt till playercontrol.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    private PlayerInput _playerInput;
+    
+    private void Awake()
+    {
+        _playerInput = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.Player.Move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Player.Move.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        var movement = _playerInput.Player.Move.ReadValue<Vector2>();
+        rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+    }
+}
