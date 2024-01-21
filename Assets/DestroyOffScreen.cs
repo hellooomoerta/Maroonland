36395f632af8538fc45ecb7cbeb409539219c771
Tommy Andersson <Tommy.J.Andersson@nexergroup.com>
2024-01-21 22:47:36 +0100

    Tagit bort bulletSpawnPoint.

--- Assets/DestroyOffScreen.cs
+++ Assets/DestroyOffScreen.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 public class DestroyOffScreen : MonoBehaviour
 {
-    private float _offset = 300f;
-    private void Update()
+    private void OnBecameInvisible()
     {
-        var screenPosition = Camera.main.WorldToScreenPoint(transform.position);
-        if (IsOffScreen(screenPosition.x, Screen.width) || IsOffScreen(screenPosition.y, Screen.height))
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private bool IsOffScreen(float position, int size)
-    {
-        return position > size + _offset || position < _offset;
+        Destroy(gameObject);
     }
 }
