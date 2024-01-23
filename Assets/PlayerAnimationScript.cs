5b5558e1587da47a93240d7cccf1558badc805bd
hellooomoerta <hilding_1989@hotmail.com>
2024-01-23 22:26:09 +0100

    8-directional animations INTE KLAR

--- Assets/PlayerAnimationScript.cs
+++ Assets/PlayerAnimationScript.cs
@@ -4,7 +4,13 @@ internal enum MovementState
 {
     Idle,
     Right,
+    DownRight,
+    Down,
+    DownLeft,
     Left,
+    UpLeft,
+    Up,
+    UpRight
 }
 
 public class PlayerAnimationScript : MonoBehaviour
@@ -19,14 +25,54 @@ public class PlayerAnimationScript : MonoBehaviour
             case MovementState.Idle:
                 animator.SetBool("isMoving", false);
             break;
-            case MovementState.Left:
-                transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+            case MovementState.Right:
                 animator.SetBool("isMoving", true);
-                break;
-            default:
                 transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+                animator.SetFloat("moveX", 1);
+                animator.SetFloat("moveY", 0);
+                break;
+            case MovementState.DownRight:
                 animator.SetBool("isMoving", true);
-            break;
+                transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+                animator.SetFloat("moveX", 0.71f);
+                animator.SetFloat("moveY", -0.71f);
+                break;
+            case MovementState.Down:
+                animator.SetBool("isMoving", true);
+                transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+                animator.SetFloat("moveX", 0);
+                animator.SetFloat("moveY", -1);
+                break;
+            case MovementState.DownLeft:
+                animator.SetBool("isMoving", true);
+                transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+                animator.SetFloat("moveX", -0.71f);
+                animator.SetFloat("moveY", -0.71f);
+                break;
+            case MovementState.Left:
+                animator.SetBool("isMoving", true);
+                transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+                animator.SetFloat("moveX", -1);
+                animator.SetFloat("moveY", 0);
+                break;
+            case MovementState.UpLeft:
+                animator.SetBool("isMoving", true);
+                transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+                animator.SetFloat("moveX", -0.71f);
+                animator.SetFloat("moveY", 0.71f);
+                break;
+            case MovementState.Up:
+                animator.SetBool("isMoving", true);
+                transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+                animator.SetFloat("moveX", 0);
+                animator.SetFloat("moveY", 1);
+                break;
+            case MovementState.UpRight:
+                animator.SetBool("isMoving", true);
+                transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+                animator.SetFloat("moveX", 0.71f);
+                animator.SetFloat("moveY", 0.71f);
+                break;
         }
     }
 
@@ -36,13 +82,37 @@ public class PlayerAnimationScript : MonoBehaviour
         {
             _movementState = MovementState.Idle;
         }
-        else if (movement.x < 0)
+        else if (movement.x == 1 && movement.y == 0)
+        {
+            _movementState = MovementState.Right;
+        }
+        else if (movement.x == 1 && movement.y == -1)
+        {
+            _movementState = MovementState.DownRight;
+        }
+        else if (movement.x == 0 && movement.y == -1)
+        {
+            _movementState = MovementState.Down;
+        }
+        else if (movement.x == -1 && movement.y == -1)
+        {
+            _movementState = MovementState.DownLeft;
+        }
+        else if (movement.x == -1 && movement.y == 0)
         {
             _movementState = MovementState.Left;
         }
-        else
+        else if (movement.x == -1 && movement.y == 1)
         {
-            _movementState = MovementState.Right;
+            _movementState = MovementState.UpLeft;
+        }
+        else if (movement.x == 0 && movement.y == 1)
+        {
+            _movementState = MovementState.Up;
+        }
+        else if (movement.x == 1 && movement.y == 1)
+        {
+            _movementState = MovementState.UpRight;
         }
     }
 }
