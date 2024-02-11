86cbdb52bb76d27168a1ddb1399eafac46db1478
hellooomoerta <hilding_1989@hotmail.com>
2024-02-11 12:02:58 +0100

    Bullet bös

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 
-public class DestroyOffScreen : MonoBehaviour
+public class BulletScript : MonoBehaviour
 {
+    [SerializeField] private GameObject bloodSplash;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(gameObject);
+            Instantiate(bloodSplash, transform.position, transform.rotation);
             collision.gameObject.SendMessage("TakeDamage");
         }
     }
