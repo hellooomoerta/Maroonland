d1f344a8f896cff496644c17e6ffd75e5d0691da
Tommy Andersson <Tommy.J.Andersson@nexergroup.com>
2024-03-02 20:33:12 +0100

    Implementerat pathfinding.

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Pathfinding;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -17,23 +18,68 @@ public class EnemyController : MonoBehaviour
     public float aimDuration = 0.05f;
     public float shootDuration = 0.2f;
     public float moveDuration = 0.3f;
-    public Rigidbody2D rb;
+   
     public PlayerAnimationScript playerAnimation;
     public WeaponScript weapon;
     private Vector2 _moveDirection = Vector2.zero;
     private Vector2 _playerDirection = Vector2.zero;
     private CharacterState _currentState = CharacterState.Moving;
     private bool _isPerformingAction = false;
+    public float nextWaypointDistance = 3f;
+    private Path _path;
+    private int _currentWaypoint = 0;
+    private bool _reachedEndOfPath = false;
+    private Seeker _seeker;
+    private Rigidbody2D _rb;
     
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
+        _seeker = GetComponent<Seeker>();
+        _rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    private void UpdatePath()
+    {
+        if (_seeker.IsDone()) {
+            _seeker.StartPath(_rb.position, player.transform.position, OnPathComplete);
+        }
+    }
+    
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            _path = p;
+            _currentWaypoint = 0;
+        }
     }
 
     private void Update()
     {
+        if (_path is null)
+        {
+            return;
+        }
+
+        if (_currentWaypoint >= _path.vectorPath.Count)
+        {
+            _reachedEndOfPath = true;
+            return;
+        }
+        _reachedEndOfPath = false;
+
+        var direction = ((Vector2)_path.vectorPath[_currentWaypoint] - _rb.position).normalized;
+        var distance = Vector2.Distance(_rb.position, _path.vectorPath[_currentWaypoint]);
+        if (distance < nextWaypointDistance)
+        {
+            _currentWaypoint++;
+        }
+        
         _playerDirection = (player.transform.position - transform.position).normalized;
-        _moveDirection = IsWithinRange() ? Vector2.zero : _playerDirection;
+        _moveDirection = IsWithinRange() ? Vector2.zero : direction;
     }
 
     private void FixedUpdate()
@@ -75,7 +121,7 @@ public class EnemyController : MonoBehaviour
     
     private void Move()
     {
-        rb.velocity = _moveDirection * moveSpeed;
+        _rb.velocity = _moveDirection * moveSpeed;
         playerAnimation.SetMovement(_moveDirection);
     }
 
