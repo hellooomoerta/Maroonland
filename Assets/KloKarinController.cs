b92cd989a62599eb327e7cfd96623cc181d994c9
hellooomoerta <hilding_1989@hotmail.com>
2024-03-02 17:27:01 +0100

    KloKarin it begins!

---
+++ Assets/KloKarinController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KloKarinController : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
